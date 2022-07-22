d5152379b2ca05bce3976e37c3057bfeba675a5d
Eduardo Mendes <eduardompereira4@outlook.com>
2022-07-22 07:35:35 -0300

    create project and add connection with postgreSQL

---
+++ controle-gado/src/main/java/br/com/uniamerica/api/controlegado/ControleGadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.api.controlegado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGadoApplication.class, args);
+	}
+
+}
---
+++ controle-gado/src/test/java/br/com/uniamerica/api/controlegado/ControleGadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.api.controlegado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
