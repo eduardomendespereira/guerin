368caa1dbec44a8e7d9e55ed5a53c1f1e6e7dd19
Eduardo Mendes <eduardompereira4@outlook.com>
2022-08-08 13:20:36 -0300

    add CRUD in VaccineApplicationService

--- src/main/java/br/com/guerin/Service/VaccineApplicationService.java
+++ src/main/java/br/com/guerin/Service/VaccineApplicationService.java
@@ -1,6 +1,15 @@
 package br.com.guerin.Service;
 
+import br.com.guerin.Entity.VaccineApplication;
+import br.com.guerin.Repository.User.VaccineApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import org.springframework.data.domain.Pageable;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * @author Eduardo Mendes
@@ -10,4 +19,38 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VaccineApplicationService {
+
+    @Autowired
+    private VaccineApplicationRepository vaccineApplicationRepository;
+
+    public Optional<VaccineApplication> findById(Long id){
+        return this.vaccineApplicationRepository.findById(id);
+    }
+
+    public Page<VaccineApplication> findAll(Pageable pageable){
+        return this.vaccineApplicationRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public void update(Long id, VaccineApplication vaccineApplication){
+        if(id == vaccineApplication.getId()){
+            this.vaccineApplicationRepository.save(vaccineApplication);
+        }else{
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void insert(VaccineApplication vaccineApplication){
+        this.vaccineApplicationRepository.save(vaccineApplication);
+    }
+
+    @Transactional
+    public void disable(Long id, VaccineApplication vaccineApplication){
+        if(id == vaccineApplication.getId()){
+            this.vaccineApplicationRepository.disable(vaccineApplication.getId());
+        }else{
+            throw new RuntimeException();
+        }
+    }
 }
