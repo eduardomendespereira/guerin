8d9744ec8654949a2de7a532bec4d80d42df3241
Lucas Hanke <lucasghank@hotmail.com>
2022-08-09 14:47:25 -0300

    fix

--- src/main/java/br/com/guerin/Controller/UserController.java
+++ src/main/java/br/com/guerin/Controller/UserController.java
@@ -1,13 +1,13 @@
 package br.com.guerin.Controller;
 
-import java.util.Date;
-import javax.servlet.ServletException;
-
 import br.com.guerin.Config.JwtConstants;
+import br.com.guerin.Payload.User.FindByEmailRequest;
 import br.com.guerin.Payload.User.LoginRequest;
 import br.com.guerin.Service.IService.IUserService;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.guerin.Entity.User;
 import io.jsonwebtoken.Jwts;
+
+import javax.servlet.ServletException;
+import java.util.Date;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost", maxAge = 3600)
 @RestController
@@ -28,7 +32,6 @@ public class UserController {
     public User registerUser(@RequestBody User user) {
         return userService.save(user);
     }
-
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(@RequestBody LoginRequest login) throws ServletException {
 
@@ -57,5 +60,26 @@ public class UserController {
                 .signWith(SignatureAlgorithm.HS256, JwtConstants.SECRET).compact();
 
         return jwtToken;
+    }
+    @RequestMapping(value = "/email", method = RequestMethod.POST)
+    public User findByEmail(@RequestBody FindByEmailRequest email) {
+        return userService.findByEmail(email.getEmail());
+    }
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public Page<User> findAll() {
+        return userService.findAll(PageRequest.of(0, 100));
+    }
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    public User updateUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public void deleteUser(@RequestBody Long id) {
+        userService.delete(id);
+    }
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public Optional<User> findById(@RequestBody Long id) {
+        return userService.findById(id);
     }
 }
