42160e09e420cdd7f0689b8e519eee72ff672e8d
Beker-Dev <joaopedro.beker@hotmail.com>
2022-08-15 22:09:07 -0300

    farmServiceTest is ok

--- src/test/java/br/com/guerin/Service/FarmServiceTest.java
+++ src/test/java/br/com/guerin/Service/FarmServiceTest.java
@@ -3,9 +3,14 @@ package br.com.guerin.Service;
 
 import br.com.guerin.Entity.Farm;
 import br.com.guerin.Service.IService.IFarmService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 
 @SpringBootTest
 public class FarmServiceTest {
@@ -23,6 +28,69 @@ public class FarmServiceTest {
 
     @Test
     public void saveFarmTest() {
-        this.farmService.save(this.farm);
+        this.farm.setName("save_name");
+        this.farm.setAddress("save_address");
+        Assertions.assertEquals(this.farmService.save(this.farm), this.farm);
     }
+
+    @Test
+    public void updateFarmTest() {
+        this.farm.setName("update_name");
+        this.farm.setAddress("update_address");
+        this.farmService.save(this.farm);
+
+        String newName = "new_update_name";
+        Farm farm = this.farmService.findByName("update_name").get();
+        farm.setName(newName);
+        Assertions.assertEquals(this.farmService.save(farm).getName(), newName);
+    }
+
+    @Test
+    public void disableFarmTest() {
+        this.farm.setName("disable_name");
+        this.farm.setAddress("disable_address");
+        Farm farm = this.farmService.save(this.farm);
+        this.farmService.disable(farm.getId());
+        farm = this.farmService.findByName("disable_name").get();
+        Assertions.assertTrue(farm.isInactive());
+    }
+
+    @Test
+    public void findByNameTest() {
+        this.farm.setName("findbyname_name");
+        this.farm.setAddress("findbyname_address");
+        Farm farm = this.farmService.save(this.farm);
+        Farm farm2 = this.farmService.findByName(farm.getName()).get();
+        Assertions.assertEquals(farm, farm2);
+    }
+
+    @Test
+    public void findByAddressTest() {
+        this.farm.setName("findbyaddress_name");
+        this.farm.setAddress("findbyaddress_address");
+        Farm farm = this.farmService.save(this.farm);
+        Farm farm2 = this.farmService.findByAddress(farm.getAddress()).get();
+        Assertions.assertEquals(farm, farm2);
+    }
+
+    @Test
+    public void findByIdTest() {
+        this.farm.setName("findbyid_name");
+        this.farm.setAddress("findbyid_address");
+        Farm farm = this.farmService.save(this.farm);
+        Farm farm2 = this.farmService.findById(farm.getId()).get();
+        Assertions.assertEquals(farm, farm2);
+    }
+
+    @Test
+    public void findAllTest() {
+        this.farm.setName("findall_name");
+        this.farm.setAddress("findall_address");
+        this.farmService.save(this.farm);
+        Integer count = this.farmService.findAll(Pageable.unpaged()).getSize();
+        Assertions.assertTrue(count >= 1);
+    }
+    // TODO: findById & findAll
+
+
 }
