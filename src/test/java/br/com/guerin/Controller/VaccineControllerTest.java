913aa89f1a889e540c0ea1d05167ec11b15ed30e
Eduardo Mendes <eduardompereira4@outlook.com>
2022-08-16 21:24:43 -0300

    add listall test in VaccineControllerTest

--- src/test/java/br/com/guerin/Controller/VaccineControllerTest.java
+++ src/test/java/br/com/guerin/Controller/VaccineControllerTest.java
@@ -1,13 +1,21 @@
 package br.com.guerin.Controller;
 
+import br.com.guerin.Entity.Vaccine;
+import br.com.guerin.Repository.Vaccine.VaccineRepository;
+import br.com.guerin.Service.IService.IVaccineService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -17,23 +25,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class VaccineControllerTest {
 
-    //UserResource user = new UserResource("Zaphod", "zaphod@galaxy.net");
 
-   //mockMvc.perform(post("/forums/{forumId}/register", 42L)
-     //   .contentType("application/json")
-       // .param("sendWelcomeMail", "true")
-       // .content(objectMapper.writeValueAsString(user)))
-         //   .andExpect(status().isOk());
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private VaccineRepository vaccineRepository;
+
     @Test
     public void checkListAll() throws Exception {
-
-        this.mockMvc.perform(get("/api/vaccines")).andDo(print()).andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().string(containsString("")));
+        Vaccine vaccine = new Vaccine();
+        vaccine.setName("carbunculo");
+        vaccine.setDate(LocalDateTime.now());
+        vaccine.setRequired(true);
+        List<Vaccine> vaccinesList = List.of(vaccine);
+        when(vaccineRepository.findAll()).thenReturn(vaccinesList);
+        this.mockMvc.perform(get("/api/vaccines"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string(containsString("carbunculo")));
     }
 
-    @Test
-    public void
+//    Users users = new Users(1L, "teste", 25, "DOC123456");
+//    List<Users> usersList = List.of(users);
+//    when(usersRepository.findAll()).thenReturn(usersList);
+//        this.mockMvc.perform(get("/users"))
+//            .andExpect(status().isOk())
+//            .andExpect(content().string(containsString("DOC123456")));
+
+
 }
