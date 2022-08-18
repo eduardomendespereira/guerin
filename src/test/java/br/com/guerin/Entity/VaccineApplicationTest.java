7881862dca01548fc34ed02bbcb86ef7b96c85b0
Eduardo Mendes <eduardompereira4@outlook.com>
2022-08-17 21:50:17 -0300

    update test VaccineApplicationEntityTest

--- src/test/java/br/com/guerin/Entity/VaccineApplicationTest.java
+++ src/test/java/br/com/guerin/Entity/VaccineApplicationTest.java
@@ -1,13 +1,26 @@
 package br.com.guerin.Entity;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.time.LocalDateTime;
 
 public class VaccineApplicationTest {
 
     private VaccineApplication vaccineApplicationFactory(){
+        Specie specie = new Specie(
+                "brandus"
+        );
+        Farm farm = new Farm("Guerin2", "rondonia");
+        Cattle cattle = new Cattle(
+                123L,
+                300F,
+                specie,
+                farm,
+                Gender.male,
+                124L,
+                125L
+        );
         Vaccine vaccine = new Vaccine();
         vaccine.setName("Raiva");
         vaccine.setDate(LocalDateTime.now());
@@ -16,18 +29,33 @@ public class VaccineApplicationTest {
         vaccineApplication.setNote("Aplicação de vacina para raiva");
         vaccineApplication.setDate(LocalDateTime.now());
         vaccineApplication.setVaccine(vaccine);
+        vaccineApplication.setCattle(cattle);
         return vaccineApplication;
     }
 
     private final VaccineApplication vaccineApplication = vaccineApplicationFactory();
 
     @Test
+    @DisplayName("Verifica se a data da aplicacao nao e futura")
     public void testDateIsNotFuture(){
         Assertions.assertFalse(vaccineApplication.dateIsFuture());
     }
 
     @Test
+    @DisplayName("Verifica se nao e inativo por default")
     public void testIsNotInactiveByDefault(){
         Assertions.assertFalse(this.vaccineApplication.isInactive());
+    }
+
+    @Test
+    @DisplayName("Verifica se o gado da aplicacao nao e nulo")
+    public void testIfCattleIsNotNull(){
+        Assertions.assertNotNull(vaccineApplication.getCattle());
+    }
+
+    @Test
+    @DisplayName("Verifica se a vacina da aplicacao nao e nula")
+    public void testIfVaccineIsNotNull(){
+        Assertions.assertNotNull(vaccineApplication.getVaccine());
     }
 }
