2e3cc212c38ebdc437587e0f65432157abb5a36c
Eduardo Mendes <eduardompereira4@outlook.com>
2022-08-20 13:16:01 -0300

    add chackListAll in VaccineControllerTest

---
+++ src/test/java/br/com/guerin/Controller/VaccineControllerTest.java
@@ -0,0 +1,55 @@
+package br.com.guerin.Controller;
+
+import br.com.guerin.Entity.Role;
+import br.com.guerin.Entity.User;
+import br.com.guerin.Service.IService.IUserService;
+import br.com.guerin.Utils.GetToken;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.HttpHeaders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class VaccineControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private VaccineController vaccineController;
+
+    @Autowired
+    private IUserService userService;
+
+    private final GetToken getToken = new GetToken();
+
+    private User userFactory(){
+        User user = new User(
+                "falano",
+                "de ciclano",
+                "ciclano@email.com",
+                "ciclano",
+                "123",
+                Role.admin
+        );
+        return this.userService.save(user);
+    }
+
+    @Test
+    public void checkListAll(){
+        User user = this.userFactory();
+        System.out.println(user);
+        String token = this.getToken.getToken(user, "123");
+        try{
+            this.mockMvc.perform(get("/api/vaccines").header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                    .andExpect(status().isOk());
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
