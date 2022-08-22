0d5b56810da67375625be64c3a841b1f4fdd83be
Eduardo Mendes <eduardompereira4@outlook.com>
2022-08-22 18:05:54 -0300

    add listAll test in CattleEventControllerTest

--- src/test/java/br/com/guerin/Controller/VaccineApplicationControllerTest.java
+++ src/test/java/br/com/guerin/Controller/VaccineApplicationControllerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.HttpHeaders;
@@ -19,8 +18,6 @@ import org.springframework.test.web.servlet.MvcResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
