f89db4f6b15a903dac82509f1f041df7314aa424
Lucas Hanke <lucasghank@hotmail.com>
2022-08-22 20:43:16 -0300

    change on service test

--- src/test/java/br/com/guerin/Service/ParameterServiceTest.java
+++ src/test/java/br/com/guerin/Service/ParameterServiceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -23,38 +25,59 @@ public class ParameterServiceTest {
         parameter.setId("PARAMETRO_TESTE");
         parameter.setValue("Sim");
         parameter.setDescription("Parametro de teste");
-        parameter.setIncluded(LocalDateTime.now());
         return parameter;
     }
-
+    private Parameter parameterNewFactory() {
+        Parameter parameter = new Parameter();
+        parameter.setId("PARAMETRO_TESTE2");
+        parameter.setValue("Nao");
+        parameter.setDescription("Parametro de teste 2");
+        return parameter;
+    }
     private final Parameter parameter = this.parameterFactory();
 
     @Test
     public void save() {
-        var obj = parameterService.save(parameter);
-        Assertions.assertEquals(obj.getId(), parameter.getId());
-        Assertions.assertEquals(obj.getValue(), parameter.getValue());
-        Assertions.assertEquals(obj.getDescription(), parameter.getDescription());
+        var newParam = this.parameterNewFactory();
+        var obj = parameterService.save(newParam);
+        Assertions.assertEquals(obj.getId(), newParam.getId());
+        Assertions.assertEquals(obj.getValue(), newParam.getValue());
+        Assertions.assertEquals(obj.getDescription(), newParam.getDescription());
     }
 
     @Test
+    public void saveDuplicated() {
+        if (!parameterService.findById(parameter.getId()).isPresent())
+            parameterService.save(parameter);
+        try {
+            var duplicatedParameter = parameterService.save(parameter);
+        } catch (ResponseStatusException ex) {
+            Assertions.assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, ex.getStatus());
+        }
+    }
+    @Test
     public void update() {
-        var p = parameterService.save(parameter);
+        if (!parameterService.findById(parameter.getId()).isPresent())
+            parameterService.save(parameter);
         var updated_parameter = parameterService.findById(parameter.getId());
         updated_parameter.get().setValue("Nao");
         var obj = parameterService.save(updated_parameter.get());
         Assertions.assertEquals(obj.getValue(), updated_parameter.get().getValue());
         Assertions.assertEquals(obj.getId(), updated_parameter.get().getId());
     }
+
     @Test
     public void findById() {
-        var p = parameterService.save(parameter);
+        if (!parameterService.findById(parameter.getId()).isPresent())
+            parameterService.save(parameter);
         var u = parameterService.findById(parameter.getId());
         Assertions.assertEquals(u.get().getId(), u.get().getId());
     }
+
     @Test
     public void delete() {
-        var p = parameterService.save(parameter);
+        if (!parameterService.findById(parameter.getId()).isPresent())
+            parameterService.save(parameter);
         parameterService.delete(parameter.getId());
         Assertions.assertEquals(false, parameterService.findById(parameter.getId()).isPresent());
     }
